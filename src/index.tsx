161270f7ff59753ab913223f3536c6c779a9e5db
Liam Tsang <lt1800@nyu.edu>
2024-08-08 22:49:58 -0400

    pre move

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,62 @@
-import {getChart} from 'billboard-top-100'
+import { getChart } from 'billboard-top-100'
 import { Hono } from 'hono'
 import { html, raw } from 'hono/html'
 import { renderer } from './renderer'
+import stringComparison from 'string-comparison'
 
 const app = new Hono()
 
 app.use(renderer)
 
-const chart = await getChart('hot-100', '2024-08-01');
-console.log(chart.songs)
+let chart: any = null
+let lev = stringComparison.levenshtein
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
+  let dates = [
+    // '2010-08-01',
+    // '2011-08-01',
+    // '2012-08-01',
+    // '2013-08-01',
+    // '2014-08-01',
+    // '2015-08-01',
+    // '2016-08-01',
+    // '2017-08-01',
+    // '2018-08-01',
+    // '2019-08-01',
+    // '2020-08-01',
+    // '2021-08-01',
+    // '2022-08-01',
+    '2023-08-01'
+  ]
+  var date = dates[Math.floor(Math.random()*dates.length)];
+  chart = await getChart('hot-100', date)
+  console.log(chart.songs)
+
   return c.render(
     <>
-    <h1>Guess The Billboard Hot 100 for the week of: {chart.week}</h1>
-    <p>Current Score: <span id="score">0</span></p>
-    <p>Guesses Remaining: <span id="guesses-remaining">5</span></p>
-    <form id="guess-form" hx-post="/guess" hx-swap="afterend">
-        <input id="guess" name="guess" type="text" />
-        <button type="submit">Submit</button>
-    </form>
-    <p id="error-message" style="color: red; display: none;"></p>
-    <div id="game-over" style="display: none;">
-      <h2>Game Over!</h2>
-      <p>You've used all your guesses. Final score: <span id="final-score"></span></p>
-      <button onclick="resetGame()">Play Again</button>
-    </div>
-    <table id="guesses-table">
-    </table>
-    <script>
-      {raw`
+      <h1>Guess The Billboard Hot 100 for the week of: {chart.week}</h1>
+      <p>
+        Current Score: <span id='score'>0</span>
+      </p>
+      <p>
+        Guesses Remaining: <span id='guesses-remaining'>5</span>
+      </p>
+      <form id='guess-form' hx-post='/guess' hx-swap='afterend'>
+        <input id='guess' name='guess' type='text' />
+        <button type='submit'>Submit</button>
+      </form>
+      <p id='error-message' style='color: red; display: none;'></p>
+      <div id='game-over' style='display: none;'>
+        <h2>Game Over!</h2>
+        <p>
+          You've used all your guesses. Final score:{' '}
+          <span id='final-score'></span>
+        </p>
+        <button onclick='resetGame()'>Play Again</button>
+      </div>
+      <table id='guesses-table'></table>
+      <script>
+        {raw`
         let score = 0;
         let guessesRemaining = 5;
         let previousGuesses = new Set();
@@ -92,13 +119,14 @@ app.get('/', (c) => {
           return 3;
         }
       `}
-    </script>
+      </script>
     </>
   )
 })
 
-async function inChart(guess:string) {
-  const found = chart.songs.find((song:any) => song.title === guess)
+async function inChart(guess: string) {
+  // const found = chart.songs.find((song: any) => song.title === guess)
+  const found = chart.songs.find((song: any) => lev.similarity(guess, song.title) > 0.93)
   console.log(found)
   return found
 }
@@ -108,21 +136,23 @@ app.post('/guess', async (c) => {
   const guess = body.guess as string
   const songinChart = await inChart(guess)
 
-  return c.html(
-    html`
-      <tr id='song-return-item' data-rank='${songinChart ? songinChart.rank : "0"}'>
-        ${songinChart ? html`
-          <th><img src='${songinChart.cover}'/></th>
-          <th>${songinChart.rank}</th>
-          <th>${songinChart.title}</th>
-          <th>${songinChart.artist}</th>
-        ` : html`
-          <th colspan="4">Song not found in chart</th>
-        `}
-      </tr>
-    `
-  )
+  return c.html(html`
+    <tr
+      id="song-return-item"
+      data-rank="${songinChart ? songinChart.rank : '0'}"
+    >
+      ${songinChart
+        ? html`
+            <tr class='song-row'>
+              <th class='row-rank'>${songinChart.rank}</th>
+              <th class='row-cover'><img src="${songinChart.cover}" /></th>
+              <th class='row-title'>${songinChart.title}</th>
+              <th class='row-artist'>${songinChart.artist}</th>
+            </tr>
+          `
+        : html` <th colspan="4">Song not found in chart</th> `}
+    </tr>
+  `)
 })
-
 
 export default app
